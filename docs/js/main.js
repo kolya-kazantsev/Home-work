2a1aea2a32f15530a9c41f0e67946f22d3129112
kolya-kazantsev <133613321+kolya-kazantsev@users.noreply.github.com>
2023-10-22 11:11:49 +0700

    Add files via upload

---
+++ docs/js/main.js
@@ -0,0 +1,16 @@
+$('.francise__slider').slick({
+    arrows:false,
+    dots:true,
+});
+$('.slider__btn-prev').on('click', function (e) {
+    e.preventDefault()
+    $('.francise__slider').slick('slickPrev')
+  })
+  $('.slider__btn-next').on('click', function (e) {
+    e.preventDefault()
+    $('.francise__slider').slick('slickNext')
+  })
+  $('.francise__slider-dots').slick({
+    arrows: false,
+    waitForAnimate: false,
+  })
